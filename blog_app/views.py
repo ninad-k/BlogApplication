0787a1dc57b99aaac32373892e639b4cd3bfdf49
Ninad K <ninadkulkarni@outlook.com>
2020-04-18 05:09:53 +0530

    Added user profile and login/logout functionality

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,28 +1,15 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'NinadK',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 18, 2020',
-    },
-    {
-        'author': 'PallaviK',
-        'title': 'Blog Post 2',
-        'content': 'First post content',
-        'date_posted': 'April 18, 2020',
-    }
-]
+from .models import Post
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': "About"})
--- django_backend/settings.py
+++ django_backend/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_app.apps.BlogAppConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
