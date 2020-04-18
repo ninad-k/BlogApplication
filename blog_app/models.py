49d3ca6c8e0d482dff6a6bc562c90e4d803c08a4
Ninad K <ninadkulkarni@outlook.com>
2020-04-18 23:41:51 +0530

    Updated Blog Post application.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -12,3 +13,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
 from blog_app import views
+from blog_app.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, \
+    UserPostListView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about', views.about, name='blog-about')
 ]
