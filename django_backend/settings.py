7e24ebcc57bbbc748e7f3dca8ded8600829ac669
Ninad K <ninadkulkarni@outlook.com>
2020-04-19 03:20:41 +0530

    Updated Settings.py file.

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ')b1gbuyjda29xqb**_k&mi9zat^b*%11q24w7@*vi+&eh_1$#7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mentoringangelapp.herokuapp.com']
+ALLOWED_HOSTS = ['mentoringangelapp.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'blog_app.apps.BlogAppConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -127,5 +128,12 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
